3fce4385210375df1c9aaaf8037d55f117836fb7
chicoribas <chico.bribas@gmail.com>
2021-09-22 11:42:53 -0300

    Improve error handling
    Signed-off-by: chicoribas <chico.bribas@gmail.com>

--- packages/scafflater/template-source/index.js
+++ packages/scafflater/template-source/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   ...require("./github-client-template-source/errors"),
+  ...require("./git-template-source/errors"),
+  GitTemplateSource: require("./git-template-source"),
   GithubClientTemplateSource: require("./github-client-template-source"),
   IsomorphicGitTemplateSource: require("./isomorphic-git-template-source"),
   TemplateSource: require("./template-source"),
